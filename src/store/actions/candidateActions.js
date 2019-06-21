ca67b54779650205c24ae956f4d1cd91f697dd99
Dipankar Halder <webmail.dip@gmail.com>
2019-06-21 11:06:59 +0530

    last com

--- src/store/actions/candidateActions.js
+++ src/store/actions/candidateActions.js
@@ -3,12 +3,7 @@ import * as Types from './types';
 
 export const getCandidates = () => dispatch => {
     axios({
-        "Type": 'GET',
-        "url": 'http://gtd-api-customer-dev.peppersquare.com/django/v1/task/mock-data/',
-        "headers": {
-            "content-type": "application/json",
-            "Authorization": "Bearer: abcd-1234-efgh-5678"
-        }
+       
     })
         .then(res => {
             console.log(res.data);
